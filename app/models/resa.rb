17fd9e9e0beb8b289a06a220c9b678342631ca97
shcyiza <patick.cyiza@gmail.com>
2016-10-16 13:39:37 +0200

    deploy setup 0.2.3

--- app/models/resa.rb
+++ app/models/resa.rb
@@ -1,10 +1,3 @@
 class Resa < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :event
-
-  #Pour faire un voucher par combinaison
-  validates :resanr, uniqueness: true, if: :new_record?
-  #pour avoir un voucher par personne
-  validates :user_id, uniqueness: { scope: :event_id, if: :new_record? message: "Vous suivez déjà reservé votre place."}
 
 end
