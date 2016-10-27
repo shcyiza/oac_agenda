bbada3fd2a1db49744a0d7252224f0bd583a9122
shcyiza <patick.cyiza@gmail.com>
2016-10-27 16:33:11 +0200

    Setu contact form with mailjet

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => %("#{subject}" de "#{name}"),
+      :subject => %(#{subject} de #{name}),
       :to => "team@kagenda.net",
       :from => "admin@kagenda.net"
     }
