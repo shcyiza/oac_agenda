0339330473361cd839bc3fb500bea2c9445db566
shcyiza <patick.cyiza@gmail.com>
2017-01-11 03:18:37 +0100

    bugfix 2016

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def track_activity(trackable, action = params[:action])
     if trackable == @event
-      current_user.activities.create! action: action, trackable: trackable, trackable_name: enom, esdate: trackable.esdate, eedate: trackable.eedate
+      current_user.activities.create! action: action, trackable: trackable, trackable_name: trackable.enom, esdate: trackable.esdate, eedate: trackable.eedate
     elsif trackable == @foldate
       current_user.activities.create! action: action, trackable: trackable, datefolwd: trackable.datefolwd
     elsif trackable == @orgn
